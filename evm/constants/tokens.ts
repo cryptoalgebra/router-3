b4847b6d8471ec915f83344516b3b7050472fbf2
damnnou <skdamn@outlook.com>
2024-06-25 12:44:17 +0300

    merge: commit 3ce2e7c

--- evm/constants/tokens.ts
+++ evm/constants/tokens.ts
@@ -3,83 +3,6 @@ import { ChainId } from '../chains/src'
 
 
 export const xLayerTestnetTokens = {
-  biba18: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0x87a851c652e5d772ba61ec320753c6349bb3c1e3',
-    18,
-    'BIBA18',
-    'BIBA18',
-  ),
-  booba18: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0x87a851c652e5d772ba61ec320753c6349bb3c1e3',
-    18,
-    'BOOBA18',
-    'BOOBA18',
-  ),
-  crazy18: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0xf4646d3740d7aF3c6AB7d608B3aa2ac90393AD6C',
-    18,
-    'CRAZY18',
-    'CRAZY18',
-  ),
-  hello18: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0x22E19487D24cB4F8A54C6E364CF04a92b1A455F3',
-    18,
-    'HELLO18',
-    'HELLO18',
-  ),
-  kakao8: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0xB5a7eD21Da067F3aB7650115ac3bb409F4c26d40',
-    8,
-    'KAKAO8',
-    'KAKAO8',
-  ),
-  ponponpon8: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0xa9DaF38B8f326d09C599FE8a92Cc9A2B03083577',
-    8,
-    'PONPONPON8',
-    'PONPON8',
-  ),
-  parapam8: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0x1402f231D76fD9278B991F2f3F4F2d3B7542aE84',
-    8,
-    'PARAPAM8',
-    'PARAPAM8',
-  ),
-  chechik8: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0x263a9c38dfe2efcc7e816cb4ce811c493757815c',
-    18,
-    'CHECHIK18',
-    'CHECHIK18',
-  ),
-  sameshit18: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0xc892b6cd091ac3d3d764f603db7d5c8c05552bf5',
-    18,
-    'SAMESHIT18',
-    'SAMESHIT18',
-  ),
-  tlvl18: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0x5f8ce21ea8c5c6c159db01958a9b2fb55c1ac97c',
-    18,
-    'Tralivali18',
-    'Tralivali18',
-  ),
-  fortest18: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0x039d1335daec1ad89becd50909defb925639dddb',
-    18,
-    'FT18',
-    'FT18',
-  ),
   wokb: new ERC20Token(
     ChainId.XLAYER_TESTNET,
     '0x87a851c652e5d772ba61ec320753c6349bb3c1e3',
@@ -101,18 +24,53 @@ export const xLayerTestnetTokens = {
     'USDT',
     'USDT',
   ),
-  t1: new ERC20Token(
+  tether: new ERC20Token(
     ChainId.XLAYER_TESTNET,
-    '0x46854e78e4034e780d6e41a717d0f8acac71631a',
-    8,
-    'T1',
-    'T1',
-  ),
-  tt2: new ERC20Token(
-    ChainId.XLAYER_TESTNET,
-    '0xd38b4cba041aaf2b69148a3e92c6f11db250ebe8',
+    '0x3b67625d24700de25c46a2cf8eaec453f9215923',
     18,
-    'TT2',
-    'TT2'
-  )
+    'USDT',
+    'Tether'
+  ),
+  usdcCoin: new ERC20Token(
+    ChainId.XLAYER_TESTNET,
+    '0xa5b0ca7d49cb4228a511af6124e753997ec57115',
+    18,
+    'USDC',
+    'USDC Coin'
+  ),
+  wrappedEther: new ERC20Token(
+    ChainId.XLAYER_TESTNET,
+    '0xace5ca1a8badbe1764a8fd444f1a7e8a6870b9c7',
+    18,
+    'WETH',
+    'Wrapped Ether'
+  ),
+  loki: new ERC20Token(
+    ChainId.XLAYER_TESTNET,
+    '0xebe9a6f19fff4099bdb1db604883e8ff27eb1ca2',
+    18,
+    'LOKI',
+    'Loki'
+  ),
+  thor: new ERC20Token(
+    ChainId.XLAYER_TESTNET,
+    '0xacfa500786304c99563dfb43886456389bc8a1b6',
+    18,
+    'THOR',
+    'Thor'
+  ),
+  ironman: new ERC20Token(
+    ChainId.XLAYER_TESTNET,
+    '0x6463514c8ab0346910172ee6c183f8c572c6ee84',
+    18,
+    'IRON',
+    'Ironman'
+  ),
+  hulk: new ERC20Token(
+    ChainId.XLAYER_TESTNET,
+    '0xc76d38cc36bf53816d91e955b92625ef9dac6cb3',
+    18,
+    'HULK',
+    'Hulk'
+  ),
 }
